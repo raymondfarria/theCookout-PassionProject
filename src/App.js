c1622fea83ad9df66e94cfdc95137e3c65ad54a5
Raymond <rayskeez21@gmail.com>
2018-07-29 15:23:35 -0400

    Added more functionality

--- src/App.js
+++ src/App.js
@@ -14,31 +14,31 @@ let fakeServerData = {
         songs: [
           {name: 'In My Feelings', duration: 1245}, 
           {name:'Best I Ever Had', duration: 3183}, 
-          {name: 'Hotline Bling', duration: 9729}
+          {name: 'Hotline Bling', duration: 3880}
         ] 
       },
       {
         name: 'Discover',
         songs: [
-          {name: 'Baby Please!!!', duration: 197937}, 
-          {name: 'Dont Go', duration: 239742}, 
-          {name: 'IDK What To Do', duration: 298329}
+          {name: 'Baby Please!!!', duration: 888}, 
+          {name: 'Dont Go', duration: 2666}, 
+          {name: 'IDK What To Do', duration: 2983}
         ]
       },
       {
         name: 'New Playlists',
         songs: [
-          {name: 'I Like Noodles', duration: 12123}, 
+          {name: 'I Like Noodles', duration: 1212}, 
           {name: 'I Kinda Like Noodles', duration: 4692}, 
-          {name: 'Noodle Me Up', duration: 92732}
+          {name: 'Noodle Me Up', duration: 2000}
         ]
       },
       {
         name: 'Friends Playlists',
         songs: [
-          {name: 'I Like Cake', duration: 16381}, 
-          {name: 'Cake Is My Friend', duration: 86392}, 
-          {name: 'Cake Makes Me Fat', duration: 93637}
+          {name: 'I Like Cake', duration: 2000}, 
+          {name: 'Cake Is My Friend', duration: 1500}, 
+          {name: 'Cake Makes Me Fat', duration: 7000}
       ]
       }
     ]
@@ -67,7 +67,7 @@ class DurationCounter extends Component{
 
     return (
       <div style= {{width: "40%", display: 'inline-block'}}>
-      <h2> Total time: {totalDuration/60 }</h2>
+      <h2> Total time: {Math.round(totalDuration/60) }</h2>
       </div>
     );
   }
@@ -90,7 +90,7 @@ class Playlist extends Component{
     return(
       <div style={{...defaultStyle, width: "25%", display: 'inline-block'}}>
        <img/>
-       <h3>Playlist Name</h3>
+       <h3>{this.props.playlist.name}</h3>
        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
       </div>
     );
@@ -109,23 +109,34 @@ componentDidMount() {
   }, 100)
 }
   render() {
+    let playlistElements = []
+    if(this.state.serverData.user){
+    this.state.serverData.user.playlists.forEach(playlist =>
+      playlistElements.push(<Playlist playlist ={playlist} />)
+    )
+  }
+
     return (
       <div className="App">
       <h1 class="title">The Cookout
       </h1>
-      {this.state.serverData.user && 
+      {this.state.serverData.user ?
+        <div> 
         <h3 style={{color: textColor}}>
         {this.state.serverData.user.name}'s Playlists
-        </h3>}
+        </h3>
       <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
       <DurationCounter playlists={this.state.serverData.user.playlists}/>
       <Filter/>
-      <Playlist/>
-      <Playlist/>
-      <Playlist/>
+      {this.state.serverData.user.playlists.map(playlists =>
+        <Playlist playlist={playlists}/>
+      )}
+      </div> : <h1 style={defaultStyle}>Loading...</h1>
+      }
       </div>
     );
   }
 }
 
 export default App;
+      
