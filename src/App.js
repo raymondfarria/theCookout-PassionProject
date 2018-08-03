9a38f9076ea2fcf12368c519e5ec4327b5d725bb
Raymond <rayskeez21@gmail.com>
2018-08-03 19:26:29 -0400

    so much added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import 'typeface-roboto';
 import queryString from 'qs';
-import "@material/card/mdc-card";
+import BurgerMenu from 'react-burger-menu';
+import { slide as Menu } from 'react-burger-menu';
+
 
 
 
@@ -13,6 +14,83 @@ let defaultStyle = {
   color: textColor,
 }
 
+let styles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    left: '36px',
+    top: '36px'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenu: {
+    background: '#373a47',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmItem:{
+    display: 'block',
+    fontSize: '30px',
+    padding: '10px'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
+
+class MenuWrap extends Component{
+  constructor(props){
+    super(props);
+    this.state= {
+      hidden: false
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const sideChanged = this.props.children.props.right !== nextProps.children.props.right;
+
+    if(sideChanged) {
+      this.setState({hidden: true});
+
+      setTimeout(() =>{
+        this.show();
+      }, this.props.wait);
+    }
+  }
+
+  show(){
+    this.setState({hidden: false});
+  }
+  render() {
+    let style;
+
+    if(this.state.hidden) {
+      style = {display: 'none'};
+    }
+
+    return(
+      <div style={style} className={this.props.side}>
+      {this.props.children}
+      </div>
+    );
+  }
+}
 
 
 class PlaylistCounter extends Component{
@@ -76,12 +154,39 @@ class Playlist extends Component{
 
 
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {serverData: {},
-    filterString: ''
+    filterString: '',
+    currentMenu: 'slide',
+    menuOpen: false,
+    side: 'left'
     }
-  }
+   }
+
+   handleStateChange(state){
+     this.setState({menuOpen: state.isOpen})
+   }
+
+   closeMenu(){
+     this.setState({menuOpen: false})
+   }
+
+   toggleMenu(){
+     this.setState({menuOpen: !this.state.menuOpen})
+   }
+
+   getItems(){
+    let items = [
+          <a key="0" href="/"><i className="menu-item" /><span>Home</span></a>,
+          <a key="1" href=""><i className="menu-item" /><span>Playlists</span></a>,
+          <a key="2" href=""><i className="menu-item" /><span>Search</span></a>,
+          <a key="3" href=""><i className="menu-item" /><span>Friends</span></a>
+        ]
+      return items;
+      }
+    
+
   componentDidMount(){
      let parsed = queryString.parse(window.location.search);
      console.log(parsed);
@@ -155,44 +260,64 @@ class App extends Component {
           return matchesPlaylist || matchesSong
         }) : []
 
+        const items = this.getItems();
+        const Menu = BurgerMenu[this.state.currentMenu];
+
+        
+        
+        return (
+          <div id="outer-container" style={{height: '100%'}}>
+            <main id="page-wrap">
+              <div className="App">
+              <MenuWrap wait={20}>
+                 <Menu styles={styles} id={this.state.currentMenu} pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
+                  {items}
+                 </Menu>
+              </MenuWrap>
+                <Typography variant='display1' align='center' gutterBottom>
+                  The Cookout
+                </Typography>
+                  {this.state.user ?
+                <div> 
+                  <Typography variant='Title' >
+                  <h3 style={{color: textColor}}>
+                  {this.state.user.name}'s Playlists
+                  </h3>
+                  </Typography>
+                  <PlaylistCounter playlists={playlistToRender}/>
+                  <DurationCounter playlists={playlistToRender}/>
+                  <Filter onTextChange={text => {             
+                    console.log(text);
+                    this.setState({filterString: text})
+                      }}/>
+                      
+                    {playlistToRender.map(playlist =>
+                  <Playlist playlist={playlist}/>
+                    )}
+                    
+                </div> : <Button onClick={()=> {
+                  window.location = window.location.href.includes('localhost') 
+                  ? 'http://localhost:8888/login' 
+                  : 'https://backendcookout.herokuapp.com/login'}
+                }
+                variant="raised" color= 'primary'>
+                Sign in with Spotify
+                </Button>
+                }
+                </div>
+               </main>
+             </div>
+        );
+      }
+    }
+
+    
+    export default App;
+      
+         
+        
+       
+          
+
     
 
-    return (
-      <div className="App">
-        <Typography variant='display1' align='center' gutterBottom>
-          The Cookout
-        </Typography>
-          {this.state.user ?
-        <div> 
-          <Typography variant='Title' >
-          <h3 style={{color: textColor}}>
-          {this.state.user.name}'s Playlists
-          </h3>
-          </Typography>
-          <PlaylistCounter playlists={playlistToRender}/>
-          <DurationCounter playlists={playlistToRender}/>
-          <Filter onTextChange={text => {             
-             console.log(text);
-             this.setState({filterString: text})
-              }}/>
-              
-            {playlistToRender.map(playlist =>
-          <Playlist playlist={playlist}/>
-            )}
-
-        </div> : <Button onClick={()=> {
-          window.location = window.location.href.includes('localhost') 
-          ? 'http://localhost:8888/login' 
-          : 'https://backendcookout.herokuapp.com/login'}
-        }
-        variant="raised" color= 'primary'>
-        Sign in with Spotify
-        </Button>
-        }
-      </div>
-    );
-  }
-}
-
-export default App;
-      
---
+++ src/Constants/constants.js
@@ -0,0 +1,25 @@
+'use strict';
+
+export default{
+    SEARCH_ADD: 'search-add',
+    SEARCH_UPDATE: 'search-update',
+    SEARCH_RESET: 'search-reset',
+    PLAYLIST_ADD_TRACKS: 'playlist-add-tracks',
+    PLAYLIST_REMOVE_TRACK: 'playlist-remove-track',
+    PLAYLIST_REMOVE_TRACKS: 'playlist-remove-tracks',
+    PLAYLIST_LOADING: 'playlist-loading',
+    PLAYLIST_CREATED: 'paylist-created',
+    PLAYLIST_SAVING: 'paylist-saving',
+    PLAYLIST_FAILED: 'paylist-failded',
+    PLAYLIST_TRACK_NOT_FOUND: 'paylist-track-not-found',
+    PLAYLIST_SAVE_FAIL: 'paylist-save-fail',
+    PLAYLIST_LIMIT_429: 'playlist-limit',
+    MODAL_OPEN: 'modal-open',
+    MODAL_CLOSE: 'modal-close',
+    USER_LOGED: 'user-loged',
+    USER_TOKEN: 'user-token',
+    USER_LOGOUT: 'user-logout',
+    USER_COUNTRY: 'user-country',
+    ALERT_OPEN: 'alert-open',
+    ALERT_CLOSE: 'alert-close'
+}
